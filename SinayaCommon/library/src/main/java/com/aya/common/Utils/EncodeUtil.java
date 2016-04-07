70e5915eb8ac7598f72354c0ce02c73b140c553f
wangyanjun <wangyanjun@youaiyihu.com>
2016-04-07 15:40:36 +0800

    bintray

--- SinayaCommon/library/src/main/java/com/aya/common/Utils/EncodeUtil.java
+++ SinayaCommon/library/src/main/java/com/aya/common/Utils/EncodeUtil.java
@@ -13,50 +13,50 @@ public class EncodeUtil {
     public EncodeUtil() {
     }
 
-    public static String Encrypt(String sSrc, String sKey) throws Exception {
-        if(sKey != null && sKey.length() == 16) {
-            byte[] raw = sKey.getBytes();
-            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-            IvParameterSpec iv = new IvParameterSpec(iv.getBytes());
-            cipher.init(1, skeySpec, iv);
-            byte[] srawt = sSrc.getBytes();
-
-            int len;
-            for(len = srawt.length; len % 16 != 0; ++len) {
-                ;
-            }
-
-            byte[] sraw = new byte[len];
-
-            for(int encrypted = 0; encrypted < len; ++encrypted) {
-                if(encrypted < srawt.length) {
-                    sraw[encrypted] = srawt[encrypted];
-                } else {
-                    sraw[encrypted] = 0;
-                }
-            }
-
-            byte[] var10 = cipher.doFinal(sraw);
-            return Base64.encode(var10);
-        } else {
-            return null;
-        }
-    }
-
-    public static String Decrypt(String sSrc, String sKey) throws Exception {
-        if(sKey != null && sKey.length() == 16) {
-            byte[] raw = sKey.getBytes("ASCII");
-            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-            IvParameterSpec iv = new IvParameterSpec(iv.getBytes());
-            cipher.init(2, skeySpec, iv);
-            byte[] encrypted = Base64.decode(sSrc);
-            byte[] original = cipher.doFinal(encrypted);
-            String originalString = new String(original);
-            return originalString.trim();
-        } else {
-            return null;
-        }
-    }
+//    public static String Encrypt(String sSrc, String sKey) throws Exception {
+//        if(sKey != null && sKey.length() == 16) {
+//            byte[] raw = sKey.getBytes();
+//            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+//            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+//            IvParameterSpec iv = new IvParameterSpec(iv.getBytes());
+//            cipher.init(1, skeySpec, iv);
+//            byte[] srawt = sSrc.getBytes();
+//
+//            int len;
+//            for(len = srawt.length; len % 16 != 0; ++len) {
+//                ;
+//            }
+//
+//            byte[] sraw = new byte[len];
+//
+//            for(int encrypted = 0; encrypted < len; ++encrypted) {
+//                if(encrypted < srawt.length) {
+//                    sraw[encrypted] = srawt[encrypted];
+//                } else {
+//                    sraw[encrypted] = 0;
+//                }
+//            }
+//
+//            byte[] var10 = cipher.doFinal(sraw);
+//            return Base64.encode(var10);
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    public static String Decrypt(String sSrc, String sKey) throws Exception {
+//        if(sKey != null && sKey.length() == 16) {
+//            byte[] raw = sKey.getBytes("ASCII");
+//            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+//            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+//            IvParameterSpec iv = new IvParameterSpec(iv.getBytes());
+//            cipher.init(2, skeySpec, iv);
+//            byte[] encrypted = Base64.decode(sSrc);
+//            byte[] original = cipher.doFinal(encrypted);
+//            String originalString = new String(original);
+//            return originalString.trim();
+//        } else {
+//            return null;
+//        }
+//    }
 }
